221f05b5ec998dd5463e0e0986ffcafbaa65de57
Hrishikesh  Shinde <149988655+Rishishinde08@users.noreply.github.com>
2026-04-08 22:00:49 +0530

    final fix working version

--- tasks.py
+++ tasks.py
@@ -1,30 +1,51 @@
-def grade_easy(predicted, expected):
-    return 0.8 if predicted == expected else 0.2
+# def grade_easy(predicted, expected):
+#     return 0.8 if predicted == expected else 0.2
 
-def grade_medium(predicted, expected):
-    return 0.85 if predicted == expected else 0.3
+# def grade_medium(predicted, expected):
+#     return 0.85 if predicted == expected else 0.3
 
-def grade_hard(predicted, expected):
-    return 0.9 if predicted == expected else 0.4
+# def grade_hard(predicted, expected):
+#     return 0.9 if predicted == expected else 0.4
+
+
+# TASKS = [
+#     {
+#         "name": "easy_spam_detection",
+#         "input": "Win a FREE iPhone now!!!",
+#         "expected": "spam",
+#         "grader": grade_easy
+#     },
+#     {
+#         "name": "medium_classification",
+#         "input": "Meeting at 5 PM today",
+#         "expected": "urgent",
+#         "grader": grade_medium
+#     },
+#     {
+#         "name": "hard_classification",
+#         "input": "Lunch tomorrow?",
+#         "expected": "normal",
+#         "grader": grade_hard
+#     }
+# ]
+
+
 
 
 TASKS = [
     {
         "name": "easy_spam_detection",
         "input": "Win a FREE iPhone now!!!",
-        "expected": "spam",
-        "grader": grade_easy
+        "expected": "spam"
     },
     {
         "name": "medium_classification",
         "input": "Meeting at 5 PM today",
-        "expected": "urgent",
-        "grader": grade_medium
+        "expected": "urgent"
     },
     {
         "name": "hard_classification",
         "input": "Lunch tomorrow?",
-        "expected": "normal",
-        "grader": grade_hard
+        "expected": "normal"
     }
 ]
